5ec3fb08761beb1e69e7c7a736b2bcd5a9ad76fa
Maxray Savage <MaxraySavage@users.noreply.github.com>
2021-06-14 19:32:19 +0000

    removed redundant readline-sync imports

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,13 +15,11 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  const input = require(`readline-sync`);
   candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  const input = require(`readline-sync`);
   candidateAnswer = input.question(question).toLowerCase();
   
 }
