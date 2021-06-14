a2355be5ff890821f1c097e09f007f67db2117e0
Maxray Savage <MaxraySavage@users.noreply.github.com>
2021-06-14 15:47:12 +0000

    part 1 completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question =  "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,13 +15,19 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  const input = require(`readline-sync`);
+  candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  const input = require(`readline-sync`);
+  candidateAnswer = input.question(question);
+  if(candidateAnswer === correctAnswer){
+    console.log("You got that right!");
+  } else {
+    console.log("That is incorrect.")
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -38,7 +44,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log(`Hello, ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
