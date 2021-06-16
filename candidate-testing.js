ed051d3510dc962270820919d16205b4763202d5
Maxray Savage <MaxraySavage@users.noreply.github.com>
2021-06-16 20:00:14 +0000

    changed gradeQuiz input

--- candidate-testing.js
+++ candidate-testing.js
@@ -40,7 +40,7 @@ function askQuestion() {
   }
 }
 
-function gradeQuiz() {
+function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   let grade=0;
